c84aa6c0dd367044986b6401e545676befab177c
Nasim <138512510+Nasim-bl@users.noreply.github.com>
2025-07-30 00:55:07 +0300

    Update Binder.h

--- rbm/Binder.h
+++ rbm/Binder.h
@@ -1,11 +1,11 @@
-/***  Binder cumulant, Ver 0.5, Date: 14010227 ********************************
+/***  Binder cumulant, Ver 0.5, Date: 17 May 2022 *****************************
  ***                                                                        ***
- ***  Copyleft (ɔ) F. Bolhasani 2020-22, All lefts reserved!                ***
+ ***  Copyleft (ɔ) Nasim.Bolhasani 2020-22, All lefts reserved!             ***
  ***                                                                        ***
  ******************************************************************************
  *
  * This code is under construction. It might contain some errors.
- * So you can try it with your own risk!
+ * So you can try it at your own risk!
  */
 
 #ifndef BINDER
@@ -16,12 +16,12 @@ struct BinderCumulant {                     // Binder cumulant
   public:
       BinderCumulant();
       void sample(float x2);                // gets a new sample.
-      void init();                          // initiates the variables and set BC to zero.
+      void init();                          // initiates the variables and sets BC to zero.
       float BC(bool reset=false);           // calculates Binder cumulant. Then reset to its initial state for
                                             // a next calculation if r is true.
   private:
     int    counter;                         // shows number of samples.
-    double sum2,                            // summation which is used in calculation of 2ⁿᵈ
+    double sum2,                            // summation which is used inthe  calculation of 2ⁿᵈ
            sum4;                            // and 4ᵗʰ moments of 〈μᵢ〉.
     double bc;
 };
