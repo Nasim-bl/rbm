bfc296578f97abe1ce88a653fb973a623f263f47
Nasim <138512510+Nasim-bl@users.noreply.github.com>
2025-07-30 01:03:45 +0300

    Update mtutils.cpp

--- rbm/mtutils.cpp
+++ rbm/mtutils.cpp
@@ -1,17 +1,17 @@
 /******************************************************************************/
 /*** General Multi-thread Utility                                           ***/
 /*** Ver 0.92                                                               ***/
-/*** Date: 13980725                                                         ***/
+/*** Date: 17 Oct. 2019                                                     ***/
 /*** Copyleft (c) 2015 by Abdorreza Goodarzinick and M. Delbari, All lefts  ***/
 /*** reserved.                                                              ***/
 /*** Licensed under The GNU General Public License v3.0                     ***/
 /*** Code implemented in GNU C++ compiler (v. 4.6.3) in 64-bit UBUNTU 12.04 ***/
-/*** Run under a Intel® Core™ i3-2330M CPU @ 2.20GHz × 4 machine with 2 GB  ***/
+/*** Run under a IntelÂ® Coreâ„¢ i3-2330M CPU @ 2.20GHz Ã— 4 machine with 2 GB  ***/
 /*** RAM.                                                                   ***/
 /******************************************************************************/
 
 /* OpenMP Note:
-  code should be compiled with -fopenmp switch of gcc.
+  Code should be compiled with the -fopenmp switch of gcc.
 */
 
 #include <stdio.h>
@@ -31,17 +31,17 @@ int N_CPU = 1;                              // Shows number of CPU
 //------------------------------------------------------------------------------
 // threadlog
 
-// threadlog is seperated log file for each thread
+// threadlog is a separate log file for each thread
 ofstream threadlog;
 #pragma omp threadprivate(threadlog)        // This line makes threadlog threadsafe variable;
-                                            // each thread has its own copy of variable.
+                                            //Each thread has its own copy of the variable.
 
 // threadlog
 //------------------------------------------------------------------------------
 
-// Initialize N_CPU and threadlog. NCore shows number of used core. -1 means all cores
-// should be used. If NCore is greater than the number of current cores, N_CPU set as the
-// current number of core.
+// Initialize N_CPU and threadlog. NCore shows the number of used cores. -1 means all cores
+// should be used. If NCore is greater than the number of current cores, N_CPUis  set as the
+// current number of cores.
 
 void init_mtutils(int NCore) {
 
