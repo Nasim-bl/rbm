45dfca862cba8e08e6236e1e07401d58cb22ee1e
Nasim <138512510+Nasim-bl@users.noreply.github.com>
2025-07-30 01:00:09 +0300

    Update estJ.h

--- rbm/estJ.h
+++ rbm/estJ.h
@@ -1,11 +1,11 @@
-/***  Estimate the J(∞), Ver 1.00, Date: 14010227 *****************************
+/***  Estimate the J(∞), Ver 1.00, Date: 22 May 2022 **************************
  ***                                                                        ***
- ***  Copyleft (ɔ) F. Bolhasani 2020-22, All lefts reserved!                ***
+ ***  Copyleft (ɔ) Nasim 2020-22, All lefts reserved!                       ***
  ***                                                                        ***
  ******************************************************************************
 
  * This code is under construction. It might contain some errors.
- * So you can try it with your own risk!
+ * So you can try it at your own risk!
  */
 
 #ifndef ESTJ_H
@@ -15,7 +15,7 @@
 #include <eigen3/Eigen/Dense>
 //#include <E:\code blocks\CodeBlocks\MinGW\include\eigen3\eigen-3.4.0\Eigen\Dense>
 
-//* The coupling dyadic between two dipole with relative displacement r.
+//* The coupling dyadic between two dipoles with relative displacement r.
 Eigen::Matrix3f couplingJ(const Eigen::Vector3f& r);
 
 //* Following function gets the a and b as bases of the triangular Bravais lattice, and
